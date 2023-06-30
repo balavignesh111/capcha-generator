7ba5cd2f242ccdf235412f0256c8688d7286c416
balavignesh111 <balavignesh1018@gmail.com>
2023-06-30 22:31:08 +0530

    js part completed

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const capchaMsg = document.querySelector('.capcha-msg');
 
 //gb var
 const capchaString = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
+let finalCapcha;
 
 //function
 //random num generator
@@ -27,10 +28,49 @@ const strGenerated = function(){
   const letterIndex = randomNumber();
   return (capchaString.slice(letterIndex,letterIndex+1));
 }
+// final capcha str
+const capcha = function(){
+  const capchaStr = strGenerated()+strGenerated()+strGenerated()+strGenerated()+strGenerated()+strGenerated();
+  return capchaStr;
+}
+
+function init(){
+  capchaMsg.style.display = 'none';
+  finalCapcha = capcha();
+  computerGeneratedCapcha.innerText = finalCapcha;
+}
+
+const result = function(str){
+  capchaMsg.style.display = 'block';
+  if(str === finalCapcha){
+    if(capchaMsg.classList.contains('fai;lure')){
+      capchaMsg.classList.remove('failure');  
+    }
+    capchaMsg.classList.add('success');
+    capchaMsg.innerText = `Nice. You don't appear to be robot`;
+  }else if(str === ''){
+    if(capchaMsg.classList.contains('success')){
+      capchaMsg.classList.remove('success');  
+    }
+    capchaMsg.classList.add('failure');
+    capchaMsg.innerText = `Please type capcha!`;
+  }else{
+    if(capchaMsg.classList.contains('success')){
+      capchaMsg.classList.remove('success');  
+    }
+    capchaMsg.classList.add('failure');
+    capchaMsg.innerText = `Captcha not matched. Please try again!`
+  }
+}
 
 
-// capcha string
-const capchaStr = strGenerated()+strGenerated()+strGenerated()+strGenerated()+strGenerated()+strGenerated();
-console.log(capchaStr);
 
 //eventlistener
+resetBtn.addEventListener('click',()=>{
+  init();
+})
+
+checkBtn.addEventListener('click',()=>{
+  result(userInput.value);
+})
+init();
